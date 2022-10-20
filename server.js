ebdf3243859bfe2c9befab4ef57cff5925dbff61
tommyyeung <tommyyeungxd@gmail.com>
2022-10-20 01:20:59 -0400

    fix minor issue->js connection to add page

--- server.js
+++ server.js
@@ -6,15 +6,15 @@
 
 
 
-/*
+
 let indexRouter = require('./server/routes/index.server.router.js');
 let contactRouter = require('./server/routes/contact.server.router');
-*/
+
 const app = require('./server/config/app');
-/*
+
 app.use('/', indexRouter);
 app.use('/contact-list', contactRouter);
-*/
+
 var HOST = 'localhost'
 const port = (process.env.PORT || '3000');
 app.listen(port, console.log(`Server running at http://${HOST}:${port}/`));
